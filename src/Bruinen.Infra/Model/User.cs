0b3592724a883506b1ab1c76fe5fd849fbcbabae
Danil <git@brcloud.io>
2026-03-01 08:09:36 +0200

    Fix: Save changed password

--- src/Bruinen.Infra/Model/User.cs
+++ src/Bruinen.Infra/Model/User.cs
@@ -3,6 +3,6 @@ namespace Bruinen.Data.Model;
 public class User
 {
     public required string Login { get; init; }
-    public required string PasswordHash { get; init; }
+    public required string PasswordHash { get; set; }
     public DateTime PasswordChangedAt { get; set; }
 }
